46e8b87d87fb8275dc39b0d3b5e8863c978be313
lordcerebrimbor <kaustubh.chaturvedi2007@gmail.com>
2026-09-06 11:08:23 +0530

    Update cash_flow.py

--- cash_flow.py
+++ cash_flow.py
@@ -5,4 +5,52 @@ def format_money(value):
     else:
         return f"${value:.2f}M"
 
-        
+#Operating Forecast Inputs
+revenues = [1000,1080,1166.4,1259.712,1360.48896,1469.3280768]
+ebitdas = [200,216,233.28,251.9424,272.097792,293.86561536]
+
+#Cash flow assumptions
+tax_rate = 0.25 #current tax rate is between 10 to 37 percent for usa
+capex_percent_revenue = 0.03
+nwc_percent_revenue = 0.01
+initial_debt = 1000
+interest_rate = 0.08       
+
+#Cash flow calculation
+free_cash_flows = []
+for year in range(1, 6):
+
+    revenue = revenues[year]
+    ebitda = ebitdas[year]
+
+    # Interest expense
+    interest = initial_debt * interest_rate
+
+    # Tax calculation
+    taxable_income = ebitda - interest
+    taxes = taxable_income * tax_rate
+
+    # Capital expenditure
+    capex = revenue * capex_percent_revenue
+
+    # Change in Net Working Capital
+    change_nwc = revenue * nwc_percent_revenue
+
+    # Free Cash Flow
+    free_cash_flow = (ebitda- interest- taxes- capex- change_nwc)
+
+    free_cash_flows.append(free_cash_flow)
+    
+
+
+print("CASH FLOW")    
+
+print(
+    f"{'Year':<8}"
+    f"{'EBITDA':>15}"
+    f"{'Interest':>15}"
+    f"{'Taxes':>15}"
+    f"{'CapEx':>15}"
+    f"{'ΔNWC':>15}"
+    f"{'FCF':>15}"
+)
