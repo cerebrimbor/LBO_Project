54ce4d521b9e8d870639cdfe26422865ba933525
lordcerebrimbor <kaustubh.chaturvedi2007@gmail.com>
2026-09-06 23:42:24 +0530

    Create debt_schedule.py

---
+++ debt_schedule.py
@@ -0,0 +1,55 @@
+#Dynamic Debt Schedule
+def format_money(value):
+    if abs(value) >= 1000:
+        return f"${value / 1000:.2f}B"
+    else:
+        return f"${value:.2f}M"
+
+#Operating Forecast Inputs
+revenues = [1000,1080,1166.4,1259.712,1360.48896,1469.3280768]
+ebitdas = [200,216,233.28,251.9424,272.097792,293.86561536]      
+
+#Cash flow assumptions
+tax_rate = 0.25 
+capex_percent_revenue = 0.03
+nwc_percent_revenue = 0.01
+initial_debt = 1000
+interest_rate = 0.08    
+
+#Debt Schedule
+beginning_debt = initial_debt
+debt_schedule = []
+
+for year in range(1, holding_period + 1):
+
+    revenue = revenues[year]
+    ebitda = ebitdas[year]
+    
+    interest = beginning_debt * interest_rate
+
+    taxable_income = ebitda - interest
+    taxes = taxable_income * tax_rate
+
+    capex = revenue * capex_percent_revenue
+
+    change_nwc = revenue * nwc_percent_revenue
+
+    free_cash_flow = (ebitda- interest- taxes- capex- change_nwc)
+
+    debt_repayment = min(free_cash_flow, beginning_debt)
+
+    ending_debt = beginning_debt - debt_repayment
+
+    # Store results
+    debt_schedule.append({
+        "year": year,
+        "beginning_debt": beginning_debt,
+        "interest": interest,
+        "free_cash_flow": free_cash_flow,
+        "debt_repayment": debt_repayment,
+        "ending_debt": ending_debt
+    })
+
+    # Next year beginning debt
+    beginning_debt = ending_debt
+ 
