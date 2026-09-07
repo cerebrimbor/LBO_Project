69ed5adb936025006ee1269e434174d3c64b2127
lordcerebrimbor <kaustubh.chaturvedi2007@gmail.com>
2026-09-07 18:22:32 +0530

    Create assumptions.py

---
+++ assumptions.py
@@ -0,0 +1,28 @@
+#ASSUMPTIONS
+
+#target company
+revenue = 1000
+ebitda = 200
+existing_debt = 300
+cash = 50
+
+#Acquisition
+entry_multiple = 10
+transaction_fees = 50
+
+#Financing
+debt_multiple = 5
+interest_rate = 0.08
+
+#Operating Forecast
+holding_period = 5
+revenue_growth = 0.08
+ebitda_margin = 0.20
+
+#Cash flow
+tax_rate = 0.25
+capex_percent_revenue = 0.03
+nwc_percent_revenue = 0.01
+
+#Exit multiple
+exit_multiple = 10
