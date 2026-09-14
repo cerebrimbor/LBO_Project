eccf11b523ab5032d95117d4ba7ff666221a7d4f
lordcerebrimbor <kaustubh.chaturvedi2007@gmail.com>
2026-09-14 18:11:15 +0530

    updated operating forecast.py

--- operating_forecast.py
+++ operating_forecast.py
@@ -1,18 +1,20 @@
 from assumptions import revenue, ebitda
-from regime_engine import economic_path
 
-# Operating Forecast
 
-revenues = [revenue]
-ebitdas = [ebitda]
+def simulate_operating_forecast(economic_path):
 
-for year in economic_path:
+    revenues = [revenue]
+    ebitdas = [ebitda]
 
-    growth = year["revenue_growth"]
-    margin = year["ebitda_margin"]
+    for year in economic_path:
 
-    next_revenue = revenues[-1] * (1 + growth)
-    next_ebitda = next_revenue * margin
+        growth = year["revenue_growth"]
+        margin = year["ebitda_margin"]
 
-    revenues.append(next_revenue)
-    ebitdas.append(next_ebitda)
+        next_revenue = revenues[-1] * (1 + growth)
+        next_ebitda = next_revenue * margin
+
+        revenues.append(next_revenue)
+        ebitdas.append(next_ebitda)
+
+    return revenues, ebitdas
